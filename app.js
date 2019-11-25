7812a0627f8665876aca1278c20e454ba094fb4a
Samuel Ramos <samuelramox@outlook.com>
2019-11-25 01:03:40 -0300

    feat: add body-parser and mongoose

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const url =
+  'mongodb+srv://admin:<password>@clusterapi-wsstp.mongodb.net/test?retryWrites=true&w=majority';
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  poolSize: 5,
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+};
+
+mongoose.connect(url, options);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('connected', () => {
+  console.log('Aplicacão conectada ao banco de dados');
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Aplicacão desconectada do banco de dados');
+});
+mongoose.connection.on('error', err => {
+  console.log('Erro na conexão com o banco de dados', err);
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const indexRoute = require('./routes/index');
 const usersRoute = require('./routes/users');
