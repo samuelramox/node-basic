b7920f6841f23f81e3dc8e72ceff1eb83af28d49
Samuel Ramos <samuelramox@outlook.com>
2019-11-25 04:06:34 -0300

    feat: add middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth');
 
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
+  console.log('userId', res.locals.auth_data);
   return res.send({ message: 'Tudo ok com o método GET da raiz!' });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Users = require('../model/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Users = require('../model/user');
+
+const createUserToken = (userId) => {
+  return jwt.sign({id: userId}, '<password>', {expiresIn: '7d'})
+}
 
 router.get('/', async (req, res) => {
   try {
@@ -22,7 +27,7 @@ router.post('/create', async (req, res) => {
 
     const user = await Users.create(req.body);
     user.password = undefined;
-    return res.send(user);
+    return res.send({user, token: createUserToken(user.id)});
   } catch (err) {
     if (err) return res.send({ error: 'Erro ao buscar usuário' });
   }
@@ -40,7 +45,7 @@ router.post('/auth', (req, res) => {
     if(!pass_ok) return res.send({ error: 'Erro ao autenticar usuário' });
 
     user.password = undefined;
-    return res.send(user);
+    return res.send({user, token: createUserToken(user.id)});
   } catch (error) {
     if (err) return res.send({ error: 'Erro ao buscar usuário' });
   }
